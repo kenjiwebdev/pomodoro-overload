6a0f1c907b33a8873d15619d701c4c24e5c0b3e6
Keneth Ade <kenjiwebdev@gmail.com>
2020-06-12 12:46:35 +0800

    Added new CSS rule

--- src/components/PomodoroStatistics.js
+++ src/components/PomodoroStatistics.js
@@ -1,33 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-class PomodoroStatistics extends React.Component {
-  
-  constructor(props) {
-  	super(props);
-  	this.state = {
-		pomodoroHowMany: 0,
-		CompletedTask: "",
-		}
-  }
+function PomodoroStatistics (props) {
+	const [show, setShow] = useState(false);
 
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
-  render() {
     return (
     	<div>
     	 <Button 
 		  className="stats-btn" 
 		  variant="outline-dark" 
 		  size="sm"
+		  onClick={handleShow}
 		  >
 		  Stats
 		  </Button>
 
+		 <Modal show={show} onHide={handleClose} size="sm" centered>
+         	<Modal.Header closeButton>
+          		<Modal.Title>Pomodoro Stats</Modal.Title>
+        	</Modal.Header>
+
+        	<Modal.Body>
+        	Woohoo, you're reading this text in a modal!
+        	</Modal.Body>
+      	 </Modal>
 		 
 		</div>
     	)
-  }
 }
 
 
--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -41,7 +41,7 @@ class TaskCard extends React.Component {
 		>
 			<Card.Header>Task:</Card.Header>
 			<Card.Body>
-			<Card.Text>
+			<Card.Text className="text-task">
       			{this.state.task}
    			</Card.Text>
    			<form>
@@ -51,12 +51,11 @@ class TaskCard extends React.Component {
    					onChange={this.handleChange}
    					style={{ marginBottom: '15px' }}/>
    				}
-
-   				{!this.state.defaultTask
-   					? <Button variant="light" onClick={this.changeTask}>Change</Button> 
-   					: <Button variant="light" onClick={this.editTask}>Edit Task</Button>
-   				}
 			</form>
+					{!this.state.defaultTask
+   					? <Button variant="light" onClick={this.changeTask} className="task-btn">Change</Button> 
+   					: <Button variant="light" onClick={this.editTask} className="task-btn">Edit Task</Button>
+   					}
 			</Card.Body>
 		</Card>
 		)
