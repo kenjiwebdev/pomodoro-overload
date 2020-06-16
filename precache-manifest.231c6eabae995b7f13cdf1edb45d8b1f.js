9badf55de216be03fea22af6368229878fea8342
Keneth Ade <kenjiwebdev@gmail.com>
2020-06-16 10:27:20 +0800

    Updates

---
+++ precache-manifest.231c6eabae995b7f13cdf1edb45d8b1f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecdf618ad30a5d93b1454792a42cea8",
+    "url": "/pomodoro-overload/index.html"
+  },
+  {
+    "revision": "daf52a30d47713f1bdeb",
+    "url": "/pomodoro-overload/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d0eed1f453975652e388",
+    "url": "/pomodoro-overload/static/css/main.e0c9bbf3.chunk.css"
+  },
+  {
+    "revision": "daf52a30d47713f1bdeb",
+    "url": "/pomodoro-overload/static/js/2.a3c440a3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pomodoro-overload/static/js/2.a3c440a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0eed1f453975652e388",
+    "url": "/pomodoro-overload/static/js/main.7c3d0f7b.chunk.js"
+  },
+  {
+    "revision": "feb79da69ce393beb668",
+    "url": "/pomodoro-overload/static/js/runtime-main.daae799e.js"
+  },
+  {
+    "revision": "bc0d30551f2a2318acbcee074581c904",
+    "url": "/pomodoro-overload/static/media/BlackOpsOne-Regular.bc0d3055.ttf"
+  },
+  {
+    "revision": "c6004be49628b1226eb31b068348d24f",
+    "url": "/pomodoro-overload/static/media/LuckiestGuy-Regular.c6004be4.ttf"
+  },
+  {
+    "revision": "a3778d587b152aab9a3105de5561eae1",
+    "url": "/pomodoro-overload/static/media/switch.a3778d58.mp3"
+  }
+]);
